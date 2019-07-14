3d0dc6834e8e4701fcfb7a551696ad7b47ef8ef7
john ornelas <johnnyoh1988@gmail.com>
2019-07-14 16:16:48 -0700

    fix: typo

--- redux/reducers/locationReducers.ts
+++ redux/reducers/locationReducers.ts
@@ -1,12 +1,12 @@
 import { Location } from '../actions';
 import { ActionTypes, Action } from '../types';
 
-const DEFAULT_EMPLOYEE_STATE = {
+const DEFAULT_LOCATION_STATE = {
   name: '',
   latitude: '',
   longitude: '',
 }
-export const currentLocation = (state:Location = DEFAULT_EMPLOYEE_STATE, action:Action) => {
+export const currentLocation = (state:Location = DEFAULT_LOCATION_STATE, action:Action) => {
   switch(action.type) {
     case ActionTypes.SET_LOCATION:
       return action.payload;
