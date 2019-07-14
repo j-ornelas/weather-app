4e0ab01a32cf4756e8466a95020b1030f14c4d7f
john ornelas <johnnyoh1988@gmail.com>
2019-07-14 16:45:42 -0700

    feat:Home - check location on component did mount

--- components/home/Home.tsx
+++ components/home/Home.tsx
@@ -1,21 +1,40 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Location, Permissions } from 'expo';
 import { connect } from 'react-redux';
 import { Dispatch } from 'redux';
 import { StoreState } from '../../redux/reducers';
-import { setLocation, Location } from '../../redux/actions';
+import { setLocation, LocationInterface } from '../../redux/actions';
 
 interface HomeProps {
   setLocation:Function;
+  currentLocation: LocationInterface;
 }
 export class HomeComponent extends React.Component<HomeProps> {
-  componentDidMount() {
-    // TODO: get initial location and set it.
+  async componentDidMount() {
+    // TODO: handle rejection gracefully.
+    const currentLocation = await this.getLocation();
+    const { coords } = currentLocation;
+    if (coords.latitude && coords.longitude) {
+      this.setLocation(coords);
+    }
   }
+
+  async getLocation() {
+    const { status } = await Permissions.askAsync(Permissions.LOCATION);
+    if (status === 'granted') {
+      return Location.getCurrentPositionAsync({ accuracy: 3 });
+    }
+  }
+
+  setLocation(location:LocationInterface) {
+    this.props.setLocation(location);
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>this is the home view.</Text>
+        <Text>your location is {this.props.currentLocation.latitude}, {this.props.currentLocation.longitude}</Text>
       </View>
     );
   }
@@ -25,7 +44,7 @@ const mapStateToProps = ({ currentLocation }:StoreState) => ({
   currentLocation,
 })
 const mapActionsToProps = (dispatch:Dispatch) => ({
-  setLocation(location:Location) {
+  setLocation(location:LocationInterface) {
     return dispatch(setLocation(location));
   },
 });
