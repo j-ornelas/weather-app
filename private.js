0b4591767cc2478c1804e61be12bfefe2b4951f7
john ornelas <johnnyoh1988@gmail.com>
2019-07-14 20:26:43 -0700

    chore: expose WEATHER_KEY variable

---
+++ private.js
@@ -0,0 +1,5 @@
+/* dev note: ideally in a real app, any private API keys would NOT be included
+in the app bundle. We'd most likely make a request to a server that had this key in a .env file,
+but that's outside the time scope of this demo.*/
+
+export const WEATHER_KEY = '<YOUR API KEY HERE>';
