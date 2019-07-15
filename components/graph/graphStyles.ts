6ab5a7b2840c2672ccc4a5f6be83111b1dfdda96
john ornelas <johnnyoh1988@gmail.com>
2019-07-14 20:10:32 -0700

    feat:graph add graph component

---
+++ components/graph/graphStyles.ts
@@ -0,0 +1,16 @@
+import styled from 'styled-components/native';
+
+export const GraphContainer = styled.View`
+  display: flex;
+  width: 100%;
+  height: 400px;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const GraphTypeButtons = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  padding-bottom: 25px;
+  width: 80%;
+`;
