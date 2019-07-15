97bdf3ee1207645395bf5f735a740032c9fcfab9
john ornelas <johnnyoh1988@gmail.com>
2019-07-14 18:51:39 -0700

    feat: header - header init

---
+++ components/header/headerStyles.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components/native';
+import { colors } from '../../utils/brand';
+
+export const HeaderContainer = styled.View`
+  display: flex;
+  width: 100%;
+  height: 150px;
+  justify-content: center;
+  align-items: center;
+  background-color: ${colors.PURPLE}
+`;
